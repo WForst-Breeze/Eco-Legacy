44f84e10431c66351340b40893951937eacda602
wforst-breeze <breezeblz@hotmail.com>
2023-07-24 13:26:39 +0800

    add refiner exploded when overload

--- src/main/java/wfb/ecolegacy/procedures/HighEnergyRefinerOpenGUIProcedure.java
+++ src/main/java/wfb/ecolegacy/procedures/HighEnergyRefinerOpenGUIProcedure.java
@@ -4,6 +4,9 @@ import wfb.ecolegacy.world.inventory.GUIHighEnergyRefinerMenu;
 
 import net.minecraftforge.network.NetworkHooks;
 
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.Vec2;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.entity.player.Player;
@@ -11,9 +14,12 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.MenuProvider;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.core.BlockPos;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.CommandSource;
 
 import io.netty.buffer.Unpooled;
 
@@ -21,20 +27,32 @@ public class HighEnergyRefinerOpenGUIProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
 		if (entity == null)
 			return;
-		{
-			if (entity instanceof ServerPlayer _ent) {
-				BlockPos _bpos = BlockPos.containing(x, y, z);
-				NetworkHooks.openScreen((ServerPlayer) _ent, new MenuProvider() {
-					@Override
-					public Component getDisplayName() {
-						return Component.literal("GUIHighEnergyRefiner");
-					}
+		if ((world.getBlockState(BlockPos.containing(x + 1, y, z))).getMaterial() == net.minecraft.world.level.material.Material.WATER
+				&& (world.getBlockState(BlockPos.containing(x - 1, y, z))).getMaterial() == net.minecraft.world.level.material.Material.WATER
+				&& (world.getBlockState(BlockPos.containing(x, y, z + 1))).getMaterial() == net.minecraft.world.level.material.Material.WATER
+				&& (world.getBlockState(BlockPos.containing(x, y, z - 1))).getMaterial() == net.minecraft.world.level.material.Material.WATER) {
+			{
+				if (entity instanceof ServerPlayer _ent) {
+					BlockPos _bpos = BlockPos.containing(x, y, z);
+					NetworkHooks.openScreen((ServerPlayer) _ent, new MenuProvider() {
+						@Override
+						public Component getDisplayName() {
+							return Component.literal("GUIHighEnergyRefiner");
+						}
 
-					@Override
-					public AbstractContainerMenu createMenu(int id, Inventory inventory, Player player) {
-						return new GUIHighEnergyRefinerMenu(id, inventory, new FriendlyByteBuf(Unpooled.buffer()).writeBlockPos(_bpos));
-					}
-				}, _bpos);
+						@Override
+						public AbstractContainerMenu createMenu(int id, Inventory inventory, Player player) {
+							return new GUIHighEnergyRefinerMenu(id, inventory, new FriendlyByteBuf(Unpooled.buffer()).writeBlockPos(_bpos));
+						}
+					}, _bpos);
+				}
+			}
+		} else {
+			world.setBlock(BlockPos.containing(x, y, z), Blocks.AIR.defaultBlockState(), 3);
+			for (int index0 = 0; index0 < 45; index0++) {
+				if (world instanceof ServerLevel _level)
+					_level.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null).withSuppressedOutput(),
+							"summon tnt");
 			}
 		}
 	}
